db6ca5ce14d5bb93810383f37c335bd663754d05
tommy280 <66056115+tommy280@users.noreply.github.com>
2020-06-25 14:58:54 +0900

    Update lamp_0.py

--- lamp_0.py
+++ lamp_0.py
@@ -1,6 +1,10 @@
-import MySQLdb
+from flask import *
+import pymysql.cursors
 
-conn = MySQLdb.connect(
+pymysql.install_as_MySQLdb()
+app = Flask(__name__)
+
+conn = pymysql.connect(
  user='root',
  passwd='passwordmysql',
  host='localhost',
@@ -21,7 +25,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return result
+    return jsonify({cursor.description[i][0]: data[0][i]
+                    for i in range(0, len(data[0]))})
 
 if __name__ == "__main__":
     app.run(debug=true)
